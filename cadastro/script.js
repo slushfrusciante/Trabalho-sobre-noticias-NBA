ecc3ca01092b4ccdebea6fecc53ed0560991b917
slushfrusciante <ffmg12@hotmail.com>
2022-11-29 19:23:40 -0300

    criado botão limpar

--- cadastro/script.js
+++ cadastro/script.js
@@ -2,3 +2,21 @@ function cadastrar ()
 {
     console.log ("Oi")
 }
+function limpar ()
+{
+    var nome = document.getElementById("nome");
+    nome.value= ""
+    var cpf = document.getElementById ("cpf");
+    cpf.value= ""
+    var usuario = document.getElementById ("usuario");
+    usuario.value = ""
+    var senha = document.getElementById ("senha");
+    senha.value = ""
+    var numero = document.getElementById ("numero");
+    numero.value = ""
+    var endereco = document.getElementById("endereco"); 
+    endereco.value = ""
+    var email = document.getElementById ("email");
+    email.value = ""
+    
+}
