e0f3f9cc3786abf311db2756b3646c5d07fcbe75
slushfrusciante <ffmg12@hotmail.com>
2022-11-29 21:07:03 -0300

    criado tabelas

--- cadastro/script.js
+++ cadastro/script.js
@@ -1,6 +1,43 @@
 function cadastrar ()
 {
-    console.log ("Oi")
+    var nome = document.getElementById("nome").value;
+    var cpf = document.getElementById ("cpf").value;
+    var usuario = document.getElementById ("usuario").value;
+    var senha = document.getElementById ("senha").value;
+    var numero = document.getElementById ("numero").value;
+    var endereco = document.getElementById("endereco").value; 
+    var email = document.getElementById ("email").value;
+    var genero = document.querySelector('input[name="genero"]:checked').value;
+    
+    
+    if (nome && cpf && usuario && senha && numero && endereco && email && genero) {
+        var tabela = document.getElementById ("tabela")
+        var linha = tabela.insertRow()
+        linha.insertCell(0).innerHTML = nome
+        linha.insertCell(1).innerHTML = usuario
+        linha.insertCell(2).innerHTML = cpf
+        linha.insertCell(3).innerHTML = numero
+        linha.insertCell(4).innerHTML = email
+        linha.insertCell(5).innerHTML = endereco
+        
+        if (genero == "1"){
+            linha.insertCell(6).innerHTML = 'Masculino'
+
+        }
+        if (genero == "2"){
+            linha.insertCell(6).innerHTML = 'Feminino'
+            
+        }
+        if (genero == "3"){
+            linha.insertCell(6).innerHTML = 'Outro'
+        }
+        limpar()
+
+    }
+    else {
+        alert ("Atenção Campo Vazio!!!")
+
+    }
 }
 function limpar ()
 {
